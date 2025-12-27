e0e0b65d57345a42535ac8f27182b77914b000da
TheGreatPythonCoder123456789 <163695719+TheGreatPythonCoder123456789@users.noreply.github.com>
2025-12-26 23:35:31 -0600

    Update Start_TheAutonomouses_ForThis.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HP_Autonomous/Start_TheAutonomouses_ForThis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HP_Autonomous/Start_TheAutonomouses_ForThis.java
@@ -1,4 +1,22 @@
 package org.firstinspires.ftc.teamcode.HP_Autonomous;
 
 public class Start_TheAutonomouses_ForThis {
+
 }
+
+/*
+public static class Paths {
+
+  public PathChain Path1;
+
+  public Paths(Follower follower) {
+    Path1 = follower
+      .pathBuilder()
+      .addPath(
+        new BezierLine(new Pose(88.000, 9.000), new Pose(81.000, 22.000)) // Start Pose, New Pose
+      )
+      .setLinearHeadingInterpolation(Math.toRadians(270), Math.toRadians(-145)) //start heading, new heading
+      .build();
+  }
+}
+*/
