596992243defabfac53963b6f418e7dedf050bb2
TheGreatPythonCoder123456789 <163695719+TheGreatPythonCoder123456789@users.noreply.github.com>
2025-12-28 23:19:20 -0600

    Update Start_TheAutonomouses_ForThis.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HP_Autonomous/Start_TheAutonomouses_ForThis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HP_Autonomous/Start_TheAutonomouses_ForThis.java
@@ -15,7 +15,7 @@ public static class Paths {
       .addPath(
         new BezierLine(new Pose(88.000, 9.000), new Pose(81.000, 22.000)) // Start Pose, New Pose
       )
-      .setLinearHeadingInterpolation(Math.toRadians(270), Math.toRadians(-145)) //start heading, new heading
+      .setLinearHeadingInterpolation(Math.toRadians(270), Math.toRadians(-114)) //start heading, new heading
       .build();
   }
 }
